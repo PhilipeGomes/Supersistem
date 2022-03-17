1166afe2013804b8920d8bb7fd98866be6289288
philipeejose <philipee.barbosa@gmail.com>
2022-03-17 17:52:22 -0300

    configuração spring

---
+++ backend/src/main/java/com/ufrpe/superSystem/SuperSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ufrpe.superSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperSystemApplication.class, args);
+	}
+
+}
