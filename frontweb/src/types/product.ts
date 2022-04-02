f5bf54bd677ff46f535cfc793d7bce0546cef6f8
BergBarbosa <bergbarboza@protonmail.com>
2022-04-02 12:07:33 -0300

    integracao Front Back Produto

--- frontweb/src/types/product.ts
+++ frontweb/src/types/product.ts
@@ -1,11 +1,13 @@
-import { Category } from "./category"
+//import { Category } from "./category"
 
 export type Product = {
     id: number;
-    name: string;
-    description: string;
-    price: number;
+    nome: string;
+    valor: number;
+    qtdEstoque: number;
+    marca: string;
     imgUrl: string;
-    date: string;
-    categories: Category[];
+    descricao: string;
+    undVenda: string;
+    //categories: Category[];
 }
